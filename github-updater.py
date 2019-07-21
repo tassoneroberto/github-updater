bf6e771b6af02a7c58c9d4ebe9878626a5f290ae
Roberto Tassone <mail.tassoneroberto@gmail.com>
2019-07-21 15:17:10 +0200

    Added gitignore, Code formatted, Changed repo path

--- github-updater.py
+++ github-updater.py
@@ -4,6 +4,7 @@ import git
 from github import Github
 import urllib.parse
 
+
 def main():
     print('Checking Github credentials...')
     if os.path.isfile('credentials.txt'):
@@ -15,7 +16,7 @@ def main():
         username = input('Insert username: ')
         password = input('Insert password: ')
         if input('Do you want to save your credentials? [y/n] ').lower() == 'y':
-            credentials = {'username' : username, 'password' : password}
+            credentials = {'username': username, 'password': password}
             credentials_file = open('credentials.txt', 'w')
             credentials_file.write(json.dumps(credentials))
             credentials_file.close()
@@ -23,18 +24,21 @@ def main():
         else:
             print('Credentials not saved!')
     g = Github(username, password)
-    repo_path = os.path.dirname(os.path.realpath(__file__))
+    repo_path = os.path.abspath(os.path.join(
+        os.path.dirname(os.path.realpath(__file__)), '..'))
+    os.chdir(repo_path)
     print('Repositories folder: '+repo_path)
     for repo in g.get_user().get_repos():
-        print('Checking ['+repo.name+'] repository status...')
         if not os.path.isdir(repo.name):
-            print('New repository: cloning')
-            git.Git(repo_path).clone('https://'+urllib.parse.quote(username)+':'+urllib.parse.quote(password)+'@github.com/'+urllib.parse.quote(username)+'/'+repo.name)
+            print('Cloning ' + repo.name)
+            git.Git(repo_path).clone('https://'+urllib.parse.quote(username)+':' +
+                                     urllib.parse.quote(password)+'@github.com/'+urllib.parse.quote(username)+'/'+repo.name)
         else:
-            print('Existing repository: pulling')
+            print('Pulling ' + repo.name)
             repo = git.Repo(os.path.join(repo_path, repo.name))
             o = repo.remotes.origin
             o.pull()
+
 
 if __name__ == "__main__":
     main()
